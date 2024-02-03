176410b6c86a343a9312b00afc00e193221376a4
Ira Kirillina <irakiri2002@gmail.com>
2024-02-03 23:29:10 +0300

    add readme.md

---
+++ README.md
@@ -0,0 +1,35 @@
+## Multi-page responsive restaurant website (frontend)
+
+---
+
+#### windows:
+
+**main page**
+<img src="result1.png" style="width: 70%; height: auto;">
+
+---
+
+<img src="result2.png" style="width: 40%; height: auto;">
+
+---
+
+**menu page**
+<img src="result3.png" style="width: 40%; height: auto;">
+
+**delivery and order page**                                         
+<img src="result4.png" style="width: 40%; height: auto;">      <img src="result5.png" style="width: 40%; height: auto;">
+
+
+**sign in and log in page**
+<img src="result6.png" style="width: 40%; height: auto;"> <img src="result8.png" style="width: 40%; height: auto;">
+
+__location part__
+<img src="result7.png" style="width: 40%; height: auto;">
+
+---
+
+## Tools
+* VScode
+*  HTML
+* CSS
+* JavaScript
--- script.js
+++ script.js
@@ -7,18 +7,18 @@ $(function(){
 
 
 
-let navbar = document.querySelector('.header  .navbar');
-let menuBtn = document.querySelector('#menu-btn');
+// let navbar = document.querySelector('.header  .navbar');
+// let menuBtn = document.querySelector('#menu-btn');
 
-menuBtn.onclick = () => {
-   menuBtn.classList.toggle('fa-times');
-   navbar.classList.toggle('active');
-};
+// menuBtn.onclick = () => {
+//    menuBtn.classList.toggle('fa-times');
+//    navbar.classList.toggle('active');
+// };
 
-window.onscroll = () => {
-   menuBtn.classList.remove('fa-times');
-   navbar.classList.remove('active');
-};
+// window.onscroll = () => {
+//    menuBtn.classList.remove('fa-times');
+//    navbar.classList.remove('active');
+// };
 
 var swiper = new Swiper(".home-slider", {
    grabCursor: true,
